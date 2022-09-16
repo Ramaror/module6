58eb383d48cc58693a1f5042d4fdf3039b43dfbd
Ramaror <mail.the.ramzan@mail.ru>
2022-09-17 01:02:54 +0300

    lessons 06.03

--- internal/pkg/util.go
+++ internal/pkg/util.go
@@ -21,3 +21,33 @@ func ReverseInt(x interface{}) (int, error) {
 
 	return result, nil
 }
+
+// ContainsDuplicate - check int array for duplicate. For example [1, 1, 2, 3] -> true
+func ContainsDuplicate(nums []int) bool {
+	set := make(map[int]struct{})
+	for _, v := range nums {
+		if _, ok := set[v]; ok {
+			return true
+		}
+
+		set[v] = struct{}{}
+	}
+
+	return false
+}
+
+// IsPalindrome - check integer for polindrome. For example 101 -> true, 123 -> false
+func IsPalindrome(x int) (bool, bool) {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false, false
+	}
+
+	var revertedNum int
+	for x > revertedNum {
+		last := x % 10
+		x /= 10
+		revertedNum = revertedNum*10 + last
+	}
+
+	return x == revertedNum || x == revertedNum/10, false
+}
--- internal/pkg/util_test.go
+++ internal/pkg/util_test.go
@@ -12,3 +12,44 @@ func TestReverseInt(t *testing.T) {
 	req.NoError(err)
 	fmt.Println(x)
 }
+func TestReverseNegativeValue(t *testing.T) {
+	x, err := ReverseInt(-960)
+	req := require.New(t)
+	req.NoError(err)
+	fmt.Println(x)
+}
+func TestReverseZeroValue(t *testing.T) {
+	x, err := ReverseInt(0)
+	req := require.New(t)
+	req.NoError(err)
+	fmt.Println(x)
+}
+
+// lessons 06.03
+// Closure driven
+func TestIsPalindrome(t *testing.T) {
+	palinCase := func(x int) func(t *testing.T) {
+		return func(t *testing.T) {
+			res, _ := IsPalindrome(x)
+			fmt.Println(res)
+		}
+	}
+	t.Run("first case", palinCase(1))
+	t.Run("second case", palinCase(54))
+	t.Run("third case", palinCase(43))
+}
+
+// Table driven
+func TestContainsDuplicate(t *testing.T) {
+	tests := map[string][]int{
+		"simple":          []int{1, 2, 3},
+		"zero values":     []int{0, 0, 0},
+		"negative values": []int{-8, -5, -5},
+	}
+	for name, testCase := range tests {
+		t.Run(name, func(t *testing.T) {
+			res := ContainsDuplicate(testCase)
+			fmt.Println(res)
+		})
+	}
+}
